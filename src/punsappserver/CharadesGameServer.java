ee3c28bcc7d16ead6d5ad574e3a78ac85a363612
maciek102 <walczakmaciek@wp.pl>
2023-12-07 15:59:13 +0100

    working words game

--- src/punsappserver/CharadesGameServer.java
+++ src/punsappserver/CharadesGameServer.java
@@ -12,10 +12,11 @@ public class CharadesGameServer implements ServerListener {
     static List<Socket> clientSockets = new ArrayList<>();
     private static long lastClearTime = 0;
     private static final long CLEAR_COOLDOWN = 1000;
-    private static int countdownSeconds = 10;
+    private static int countdownSeconds = 60;
     private static boolean countdownRunning = false;
     static Map<String, Socket> userSocketMap = new HashMap<>();
     private static int drawingPlayerIndex = 0;
+    private static List<String> words = new ArrayList<>();
 
 
     public static void main(String[] args) {
@@ -23,7 +24,18 @@ public class CharadesGameServer implements ServerListener {
             ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("Charades Game Server is running on port " + PORT);
 
-            //startCountdownTimer();
+            // Load words from a file into the 'words' list
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader("words_charades.txt"));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] wordsInLine = line.split(",\\s*"); // Split words by commas and optional spaces
+                    words.addAll(Arrays.asList(wordsInLine));
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
@@ -77,6 +89,15 @@ public class CharadesGameServer implements ServerListener {
         }
     }
 
+    private static void sendToClient(String message, Socket socket) {
+        try {
+            PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
+            socketOut.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     private static void broadcastPlayerCount() {
         Message message = new Message();
         message.setMessageType("PLAYER_COUNT");
@@ -100,11 +121,23 @@ public class CharadesGameServer implements ServerListener {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            changeDrawingPlayer();
+
+            //changeDrawingPlayer();
+            Socket currentDrawingSocket = clientSockets.get(drawingPlayerIndex);
+            String randomWord = words.get(new Random().nextInt(words.size())-1);
+            String username = getUsernameForSocket(currentDrawingSocket);
+            Message message = new Message();
+            message.setMessageType("CHAT");
+            message.setChat("Turn to draw: " + username);
+            String json = new Gson().toJson(message);
+            broadcast(json);
+            notifyDrawingPlayer(currentDrawingSocket, randomWord);
+
             while (countdownSeconds >= 0) {
                 countdownSeconds--;
                 if (countdownSeconds < 0) {
                     countdownSeconds = 60; // Reset countdown to 1 minute
+                    drawingPlayerIndex++;
                     changeDrawingPlayer();
                 }
                 broadcastCountdown(countdownSeconds);
@@ -151,23 +184,32 @@ public class CharadesGameServer implements ServerListener {
     }
 
     private static void changeDrawingPlayer() {
-        drawingPlayerIndex++;
+        //drawingPlayerIndex++;
         if (drawingPlayerIndex >= clientSockets.size()) {
             drawingPlayerIndex = 0; // Reset to the first client socket if reached the end
         }
+
+        String randomWord = words.get(new Random().nextInt(words.size())-1);
+
         // Notify the current drawing player
         Socket currentDrawingSocket = clientSockets.get(drawingPlayerIndex);
-        notifyDrawingPlayer(currentDrawingSocket);
+        notifyDrawingPlayer(currentDrawingSocket, randomWord);
         clearCanvas();
     }
 
-    private static void notifyDrawingPlayer(Socket drawingSocket) {
+    private static void notifyDrawingPlayer(Socket drawingSocket, String word) {
         String username = getUsernameForSocket(drawingSocket);
         Message message = new Message();
         message.setMessageType("CHAT");
         message.setChat("Turn to draw: " + username);
         String json = new Gson().toJson(message);
         broadcast(json);
+
+        message = new Message();
+        message.setMessageType("CHAT");
+        message.setChat("Your word is: " + word);
+        json = new Gson().toJson(message);
+        sendToClient(json, drawingSocket);
 
         message = new Message();
         message.setMessageType("PERMISSION");
